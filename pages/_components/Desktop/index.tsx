32aaf3d76d0bb0508082a50a0d89258eed87249b
soroushchehresa <s1996ch@gmail.com>
2021-08-12 19:46:49 +0430

    HOTFIX

--- pages/_components/Desktop/index.tsx
+++ pages/_components/Desktop/index.tsx
@@ -46,7 +46,7 @@ const Desktop = () => {
 
     return (
       <div
-        className={`image-wrapper rounded-md border-2 border-transparent border-box px-1 py-2${
+        className={`image-wrapper rounded-md border-2 border-transparent border-box px-1 pt-2 pb-2${
           active ? ' bg-gray-900 border-gray-500 bg-opacity-30' : ''
         }`}
       >
